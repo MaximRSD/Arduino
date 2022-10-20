a0e68c020aa2b603a1d6087a1f7353947e52cec0
Maxim Jansen <45556790+MaximRSD@users.noreply.github.com>
2022-10-20 16:08:14 +0200

    Update opdracht2.ino

--- opdracht2.ino
+++ opdracht2.ino
@@ -1,32 +1,29 @@
-const int buttonPin = 7;
-const int ledPin = 11;
-int buttonState = 0;
-int counter = 1;
-int lastButtonState = 0;
+const int buttonPin = 7;                         //Buttonpin op pin 7 gezet
+const int ledPin = 11;                           //ledPin op pin 11 gezet
+int buttonState = 0;                             //Buttonstate 0 waarde gegeven
+int counter = 1;                                 //counter 1 waarde gegeven
+int lastButtonState = 0;                         //lastButtonState 0 waarde gegeven
 
 void setup() {
-  pinMode(buttonPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);                     //Uitgang van buttonPin gedefineerd als INPUT
+  pinMode(ledPin, OUTPUT);                       //Uitgang van ledPin gedefineerd als OUTPUT
 }
 
-void loop() {
-  buttonState = digitalRead(buttonPin);
+void loop() { 
+  buttonState = digitalRead(buttonPin);             //Leest de waarde van buttonPin bij buttonState
   //Buttonstate verandert als je erop drukt.
   if (buttonState != lastButtonState) {
     if (buttonState == HIGH) {
-      //telt op wanneer er gedrukt is (Voor state)
-      counter++;
+      counter++;                                    //telt op wanneer er gedrukt is (Voor state)
     }
-    delay(100);
+    delay(100);                                     //delay van 1/10 seconden
   }
   lastButtonState = buttonState;
-  //Als counter hoger of gelijk is dan is de ledpin aan
-  if (counter >= 2) {
-    digitalWrite(ledPin, HIGH);
+  if (counter >= 2) {                               //Als counter hoger of gelijk is dan is de ledpin aan                       
+    digitalWrite(ledPin, HIGH);                     //Ledpin aan
     counter = 0;
   }
-  //Als counter gelijk is aan 1 dan gaat de led uit.
-  else if (counter == 1){
-    digitalWrite(ledPin, LOW);
+  else if (counter == 1){                           //Als counter gelijk is aan 1 dan gaat de led uit.
+    digitalWrite(ledPin, LOW);                      //Ledpin uit
   }
 }
