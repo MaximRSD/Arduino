a5937016ce516e268014291d464b635b99dae54f
Maxim Jansen <45556790+MaximRSD@users.noreply.github.com>
2022-10-20 15:58:46 +0200

    Update opdracht3.ino

--- opdracht3.ino
+++ opdracht3.ino
@@ -1,99 +1,92 @@
-int potMeter = A5;
-int ledPin7 = 7;
-int ledPin8 = 8;
-int ledPin9 = 9;
-int ledPin10 =10;
-int ledPin11 = 11;
-int ledPin12 = 12;
-int ledPin13 = 13;
+int potMeter = A5;                              //Potmeter op pin A5 gezet
+int ledPin7 = 7;                                //Ledpin 7 op pin 7 gezet
+int ledPin8 = 8;                                //Ledpin 8 op pin 8 gezet
+int ledPin9 = 9;                                //Ledpin 9 op pin 9 gezet
+int ledPin10 =10;                               //Ledpin 10 op pin 10 gezet
+int ledPin11 = 11;                              //Ledpin 11 op pin 11 gezet
+int ledPin12 = 12;                              //Ledpin 12 op pin 12 gezet
+int ledPin13 = 13;                              //Ledpin 13 op pin 13 gezet
 
 void setup(){
-  pinMode(potMeter, INPUT);
-  pinMode(ledPin7, OUTPUT);
-  pinMode(ledPin8, OUTPUT);
-  pinMode(ledPin9, OUTPUT);
-  pinMode(ledPin10, OUTPUT);
-  pinMode(ledPin11, OUTPUT);
-  pinMode(ledPin12, OUTPUT);
-  pinMode(ledPin13, OUTPUT);
-  //Bijhouden Potentiemeter in terminal
-  Serial.begin(9600);
+  pinMode(potMeter, INPUT);                     //Uitgang van potMeter gedefinieerd als INPUT
+  pinMode(ledPin7, OUTPUT);                     //Uitgang van ledpin7 gedefinieerd als OUTPUT
+  pinMode(ledPin8, OUTPUT);                     //Uitgang van ledpin8 gedefinieerd als OUTPUT
+  pinMode(ledPin9, OUTPUT);                     //Uitgang van ledpin9 gedefinieerd als OUTPUT
+  pinMode(ledPin10, OUTPUT);                    //Uitgang van ledpin10 gedefinieerd als OUTPUT
+  pinMode(ledPin11, OUTPUT);                    //Uitgang van ledpin11 gedefinieerd als OUTPUT
+  pinMode(ledPin12, OUTPUT);                    //Uitgang van ledpin12 gedefinieerd als OUTPUT
+  pinMode(ledPin13, OUTPUT);                    //Uitgang van ledpin13 gedefinieerd als OUTPUT
+  Serial.begin(9600);                           //Bijhouden Potentiemeter in terminal
 }
 
 void loop(){
-int potMeting = analogRead(A5);
-  Serial.println(potMeting);
+int potMeting = analogRead(A5);                 //potMeting is de waarde die gegeven wordt bij analogRead(A5)
+  Serial.println(potMeting);                    //Print de metingen uit in de seriele monitor
   //loop door de metingen heen 
-  if(potMeting < 50){
-    digitalWrite(ledPin7, LOW);
-    digitalWrite(ledPin8, LOW);
-    digitalWrite(ledPin9, LOW);
-    digitalWrite(ledPin10, LOW);
-    digitalWrite(ledPin11, LOW);
-    digitalWrite(ledPin12, LOW);
-    digitalWrite(ledPin13, LOW);
-  }else  if(potMeting < 250){
-    digitalWrite(ledPin7, HIGH);
-    digitalWrite(ledPin8, LOW);
-    digitalWrite(ledPin9, LOW);
-    digitalWrite(ledPin10, LOW);
-    digitalWrite(ledPin11, LOW);
-    digitalWrite(ledPin12, LOW);
-    digitalWrite(ledPin13, LOW);
-  
-  }else  if(potMeting < 300){
-    digitalWrite(ledPin7, HIGH);
-    digitalWrite(ledPin8, HIGH);
-    digitalWrite(ledPin9, LOW);
-    digitalWrite(ledPin10, LOW);
-    digitalWrite(ledPin11, LOW);
-    digitalWrite(ledPin12, LOW);
-    digitalWrite(ledPin13, LOW);
-  
-  }else  if(potMeting < 450){
-    digitalWrite(ledPin7, HIGH);
-    digitalWrite(ledPin8, HIGH);
-    digitalWrite(ledPin9, HIGH);
-    digitalWrite(ledPin10, LOW);
-    digitalWrite(ledPin11, LOW);
-    digitalWrite(ledPin12, LOW);
-    digitalWrite(ledPin13, LOW);
-  
-  }else if(potMeting < 600){
-    digitalWrite(ledPin7, HIGH);
-    digitalWrite(ledPin8, HIGH);
-    digitalWrite(ledPin9, HIGH);
-    digitalWrite(ledPin10, HIGH);
-    digitalWrite(ledPin11, LOW);
-    digitalWrite(ledPin12, LOW);
-    digitalWrite(ledPin13, LOW);
-    
-  }else if(potMeting < 750){
-    digitalWrite(ledPin7, HIGH);
-    digitalWrite(ledPin8, HIGH);
-    digitalWrite(ledPin9, HIGH);
-    digitalWrite(ledPin10, HIGH);
-    digitalWrite(ledPin11, HIGH);
-    digitalWrite(ledPin12, LOW);
-    digitalWrite(ledPin13, LOW);
-    
-  }else if(potMeting < 900){
-    digitalWrite(ledPin7, HIGH);
-    digitalWrite(ledPin8, HIGH);
-    digitalWrite(ledPin9, HIGH);
-    digitalWrite(ledPin10, HIGH);
-    digitalWrite(ledPin11, HIGH);
-    digitalWrite(ledPin12, HIGH);
-    digitalWrite(ledPin13, LOW);
-    
-  }else if(potMeting < 1024){
-    digitalWrite(ledPin7, HIGH);
-    digitalWrite(ledPin8, HIGH);
-    digitalWrite(ledPin9, HIGH);
-    digitalWrite(ledPin10, HIGH);
-    digitalWrite(ledPin11, HIGH);
-    digitalWrite(ledPin12, HIGH);
-    digitalWrite(ledPin13, HIGH);
+  if(potMeting < 50){                           //Als potmeting kleiner is dan 50 voert hij de statement uit
+    digitalWrite(ledPin7, LOW);                 //Pin7 uit
+    digitalWrite(ledPin8, LOW);                 //Pin8 uit
+    digitalWrite(ledPin9, LOW);                 //Pin9 uit
+    digitalWrite(ledPin10, LOW);                //Pin10 uit
+    digitalWrite(ledPin11, LOW);                //Pin11 uit
+    digitalWrite(ledPin12, LOW);                //Pin12 uit
+    digitalWrite(ledPin13, LOW);                //Pin13 uit
+  }else  if(potMeting < 250){                   //Als potmeting kleiner is dan 250 voert hij de statement uit
+    digitalWrite(ledPin7, HIGH);                //Pin7 aan
+    digitalWrite(ledPin8, LOW);                 //Pin8 uit
+    digitalWrite(ledPin9, LOW);                 //Pin9 uit
+    digitalWrite(ledPin10, LOW);                //Pin10 uit
+    digitalWrite(ledPin11, LOW);                //Pin11 uit
+    digitalWrite(ledPin12, LOW);                //Pin12 uit
+    digitalWrite(ledPin13, LOW);                //Pin13 uit
+  }else  if(potMeting < 300){                   //Als potmeting kleiner is dan 250 voert hij de statement uit
+    digitalWrite(ledPin7, HIGH);                //Pin7 aan
+    digitalWrite(ledPin8, HIGH);                //Pin8 aan
+    digitalWrite(ledPin9, LOW);                 //Pin9 uit
+    digitalWrite(ledPin10, LOW);                //Pin10 uit
+    digitalWrite(ledPin11, LOW);                //Pin11 uit
+    digitalWrite(ledPin12, LOW);                //Pin12 uit
+    digitalWrite(ledPin13, LOW);                //Pin13 uit
+  }else  if(potMeting < 450){                   //Als potmeting kleiner is dan 450 voert hij de statement uit
+    digitalWrite(ledPin7, HIGH);                //Pin7 aan
+    digitalWrite(ledPin8, HIGH);                //Pin8 aan
+    digitalWrite(ledPin9, HIGH);                //Pin9 aan
+    digitalWrite(ledPin10, LOW);                //Pin10 uit
+    digitalWrite(ledPin11, LOW);                //Pin11 uit
+    digitalWrite(ledPin12, LOW);                //Pin12 uit
+    digitalWrite(ledPin13, LOW);                //Pin13 uit
+  }else if(potMeting < 600){                    //Als potmeting kleiner is dan 600 voert hij de statement uit
+    digitalWrite(ledPin7, HIGH);                //Pin7 aan
+    digitalWrite(ledPin8, HIGH);                //Pin8 aan
+    digitalWrite(ledPin9, HIGH);                //Pin9 aan
+    digitalWrite(ledPin10, HIGH);               //Pin10 aan
+    digitalWrite(ledPin11, LOW);                //Pin11 uit
+    digitalWrite(ledPin12, LOW);                //Pin12 uit
+    digitalWrite(ledPin13, LOW);                //Pin13 uit  
+  }else if(potMeting < 750){                    //Als potmeting kleiner is dan 750 voert hij de statement uit
+    digitalWrite(ledPin7, HIGH);                //Pin7 aan
+    digitalWrite(ledPin8, HIGH);                //Pin8 aan
+    digitalWrite(ledPin9, HIGH);                //Pin9 aan
+    digitalWrite(ledPin10, HIGH);               //Pin10 aan
+    digitalWrite(ledPin11, HIGH);               //Pin11 aan
+    digitalWrite(ledPin12, LOW);                //Pin12 uit
+    digitalWrite(ledPin13, LOW);                //Pin13 uit
+  }else if(potMeting < 900){                    //Als potmeting kleiner is dan 900 voert hij de statement uit
+    digitalWrite(ledPin7, HIGH);                //Pin7 aan
+    digitalWrite(ledPin8, HIGH);                //Pin8 aan
+    digitalWrite(ledPin9, HIGH);                //Pin9 aan
+    digitalWrite(ledPin10, HIGH);               //Pin10 aan
+    digitalWrite(ledPin11, HIGH);               //Pin11 aan
+    digitalWrite(ledPin12, HIGH);               //Pin12 aan
+    digitalWrite(ledPin13, LOW);                //Pin13 uit
+  }else if(potMeting < 1024){                   //Als potmeting kleiner is dan 1024 voert hij de statement uit
+    digitalWrite(ledPin7, HIGH);                //Pin7 aan
+    digitalWrite(ledPin8, HIGH);                //Pin8 aan
+    digitalWrite(ledPin9, HIGH);                //Pin9 aan
+    digitalWrite(ledPin10, HIGH);               //Pin10 aan
+    digitalWrite(ledPin11, HIGH);               //Pin11 aan
+    digitalWrite(ledPin12, HIGH);               //Pin12 aan
+    digitalWrite(ledPin13, HIGH);               //Pin13 aan
     
   }
 }
