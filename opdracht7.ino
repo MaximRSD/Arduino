4dc4e1762dfde007636bda44cc3568bf77c9be13
Maxim Jansen <45556790+MaximRSD@users.noreply.github.com>
2022-10-20 14:14:54 +0200

    Update opdracht7.ino

--- opdracht7.ino
+++ opdracht7.ino
@@ -1,47 +1,47 @@
 #include <Servo.h>
 
-const int trigPin = 12; // Trigger pin
-const int echoPin = 11; // Echo Pin
-const int servoPin  = 9; // Servo Pin
+const int trigPin = 12;          // Trigger pin
+const int echoPin = 11;          // Echo Pin
+const int servoPin  = 9;         // Servo Pin
 
-Servo servo;
+Servo servo;                     
 
-float time; // Variable tijdsduur
-float distance; // Variable voor de afstand
+float time;                      // Variable tijdsduur
+float distance;                  // Variable voor de afstand
 
-int angle = 0;
+int angle = 0;                   // Angle op 0 gezet
 
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(9600);            //Stel de seriele monitor in
 
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);      //Uitgang van trigPin gedefinieerd als OUTPUT
+  pinMode(echoPin, INPUT);       //Uitgang van echoPin gedeinieerd als INPUT
 
-  servo.attach(servoPin);
+  servo.attach(servoPin);        // Verbind de servo.attach aan de servoPin
  
 }
 
 void loop(){
-  digitalWrite(trigPin, LOW); // Trigpin op LOW
-  delayMicroseconds(2);
+  digitalWrite(trigPin, LOW);    // Trigpin op LOW
+  delayMicroseconds(2);          // DelayMicro gezet op 2
 
-  digitalWrite(trigPin, HIGH); // TrigPin op HIGH
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin, HIGH);   // TrigPin op HIGH
+  delayMicroseconds(10);         // DelayMicro gezet op 10
+  digitalWrite(trigPin, LOW);    // TrigPin op LOW gezet
 
   time = pulseIn(echoPin, HIGH); // Echopin op HIGH
 
-  distance = time / 58; // Bereking afstand en de snelheid van geluit
-  Serial.print("Afstand: ");
-  Serial.print(distance); // Toont de afstand op de seriele monitor
-  Serial.println("cm");
+  distance = time / 58;          // Bereking afstand en de snelheid van geluit
+  Serial.print("Afstand: ");     // Print het woord afstand in de serial monitor
+  Serial.print(distance);        // Toont de afstand op de seriele monitor
+  Serial.println("cm");          // Print het woord cm in de serial monitor
 
-  if(distance < 10 && distance >4) {
-    Serial.println("Kleiner dan 10");
-  servo.write(0);
-  }if(distance < 4) {
-    Serial.println("Kleiner dan 4");
-  servo.write(180);
+  if(distance < 10 && distance >4) {        // Kijkt of distance lager is dan 10 en hoger is dan 4
+    Serial.println("Kleiner dan 10");       // Print Kleiner dan 10 in de seriele monitor
+  servo.write(0);                           // Servo draait naar 0 graden
+  }if(distance < 4) {                       // Kijkt of distance lager is dan 4
+    Serial.println("Kleiner dan 4");        // Print kleiner dan 4 in de seriele monitor
+  servo.write(180);                         // Servo draait naar 180 graden
   }
-  delay(100);
+  delay(100);                               // Geeft een kleine delay aan van 1/10 van een seconde
 }
