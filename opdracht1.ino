f5b6408dd922a63a89f05c6ad0568baaf6f46663
Maxim Jansen <45556790+MaximRSD@users.noreply.github.com>
2022-10-20 16:13:08 +0200

    Update opdracht1.ino

--- opdracht1.ino
+++ opdracht1.ino
@@ -1,39 +1,21 @@
-int timer = 100;           
+int timer = 100;                                      //timer gezet op 100   
 
 void setup() {
-
-
-  for (int thisPin = 13; thisPin < 5; thisPin++) {
-
-    pinMode(thisPin, OUTPUT);
-
+  for (int thisPin = 13; thisPin < 5; thisPin++) {    //for loop door pinnen 
+    pinMode(thisPin, OUTPUT);                         //Uitgang van thisPin gedefineerd als OUTPUT
   }
 }
 
 void loop() {
-
-
-  for (int thisPin = 5; thisPin < 13; thisPin++) {
-
-
-    digitalWrite(thisPin, HIGH);
-
-    delay(timer);
- 
-
-    digitalWrite(thisPin, LOW);
-
+  for (int thisPin = 5; thisPin < 13; thisPin++) {   //for loop door pinnen 
+    digitalWrite(thisPin, HIGH);                     //thisPin aan
+    delay(timer);                                    //delay gesteld op timer oftewel 100
+    digitalWrite(thisPin, LOW);                      //thisPin uit
   }
 
-
-
-  for (int thisPin = 13; thisPin >= 5; thisPin--) {
-
-    digitalWrite(thisPin, HIGH);
-
-    delay(timer);
-
-    digitalWrite(thisPin, LOW);
-
+  for (int thisPin = 13; thisPin >= 5; thisPin--) {  //for loop door pinnen
+    digitalWrite(thisPin, HIGH);                     //thisPin aan
+    delay(timer);                                    //delay gesteld op timer oftewel 100
+    digitalWrite(thisPin, LOW);                      //thisPin uit
   }
 }
