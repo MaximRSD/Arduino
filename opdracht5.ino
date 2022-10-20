35953fe5fc3fa075450aebfcf8f51f4ee6e365af
Maxim Jansen <45556790+MaximRSD@users.noreply.github.com>
2022-10-20 15:43:37 +0200

    Update opdracht5.ino

--- opdracht5.ino
+++ opdracht5.ino
@@ -1,30 +1,28 @@
-const int ledR = 11;
-const int ledG = 10;
-const int ledB = 9;
-const int boost = 10;
+const int ledR = 11;        //LED rood op pin 11 gezet
+const int ledG = 10;        //LED groen op pin 10 gezet
+const int ledB = 9;         //LED blue op pin 9 gezet
+const int boost = 10;       //boost de waarde 10 gegeven
 
 void setup() {
-    pinMode(ledR, OUTPUT);
-    pinMode(ledG, OUTPUT);
-    pinMode(ledB, OUTPUT);
+    pinMode(ledR, OUTPUT);    //Uitgang van ledR gedefinieerd als OUTPUT
+    pinMode(ledG, OUTPUT);    //Uitgang van ledG gedefinieerd als OUTPUT
+    pinMode(ledB, OUTPUT);    //Uitgang van ledB gedefinieerd als OUTPUT
 }
 
 void loop() {
-  //Loop om door de kleuren heen te gaan
-    for (int redValue = 0; redValue < 255; redValue++) {
-      //De loop telt het aantal bij R in rgb
+    for (int redValue = 0; redValue < 255; redValue++) {                  //Loop om door de kleuren heen te gaan
+      //De loop telt het aantal bij R in rgb    
         analogWrite(ledR, redValue);
-        //Kleur plus boost, zodat het sneller van kleuren verandert.
-        redValue += boost;
-        delay(100);
-        for (int greenValue = 0; greenValue < 255; greenValue++) {
+        redValue += boost;                                                //Kleur plus boost, zodat het sneller van kleuren verandert.
+        delay(100);                                                       //delay gesteld van 1/10 seconden
+        for (int greenValue = 0; greenValue < 255; greenValue++) {        //Loop om door de kleuren heen te gaan
             analogWrite(ledG, greenValue);
-            greenValue += boost;
-            delay(100);
-            for (int blueValue = 0; blueValue < 255; blueValue++) {
-                analogWrite(ledB, blueValue);
-                blueValue += boost;
-                delay(100);
+            greenValue += boost;                                          //Kleur plus boost, zodat het sneller van kleuren verandert.
+            delay(100);                                                   //delay gesteld van 1/10 seconden
+            for (int blueValue = 0; blueValue < 255; blueValue++) {       //Loop om door de kleuren heen te gaan
+                analogWrite(ledB, blueValue);                     
+                blueValue += boost;                                       //Kleur plus boost, zodat het sneller van kleuren verandert.
+                delay(100);                                               //delay gesteld van 1/10 seconden
             }
         }
     }
