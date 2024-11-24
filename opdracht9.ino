574a19812714de974d3639723faf41d3985d742c
Maxim Jansen <45556790+MaximRSD@users.noreply.github.com>
2024-11-24 20:09:29 +0100

    Update opdracht9.ino
    updated code

--- opdracht9.ino
+++ opdracht9.ino
@@ -1,63 +1,69 @@
-const int buttonPin = 0;          //Knop op pin 0 gezet
-int GREEN = 5;                    //LED groen op pin 5 gezet
-int YELLOW = 6;                   //LED geel op pin 6 gezet
-int RED = 7;                      //LED rood op pin 7 gezet
-int buttonState = 0;              //Status van de knop op 0 gezet
-bool isPressed = false;           //isPressed false gezet
+#include <SoftwareSerial.h>
 
+// Bluetooth op pinnen 0 en 1
+SoftwareSerial bluetooth(0, 1);
 
-// basic functions
-void setup()
-{
-  Serial.begin(9600);             //Stel de seriele monitor in
-  pinMode(GREEN, OUTPUT);         //Uitgang van Pin gedefinieerd als OUTPUT
-  pinMode(YELLOW, OUTPUT);        //Uitgang van Pin gedefinieerd als OUTPUT
-  pinMode(RED, OUTPUT);           //Uitgang van Pin gedefinieerd als OUTPUT
-  pinMode(buttonPin, INPUT);      //Uitgang van Pin gedefinieerd als OUTPUT
-}
+// LED-pinnen en variabelen
+const int LED[] = {4, 3, 2};
+unsigned long lastTime = 0;
+unsigned long lastCheck = 0;
+bool huidigeStaat = false;
+int ledStatus = LOW;
 
-void loop()
-{
-  buttonState = digitalRead(buttonPin);       //Leest de value van de buttonPin als buttonstate
-  if(buttonState == LOW){isPressed = true;}   //Als buttonState LOW is dan wordt isPressed True
-  if (isPressed == true){                     //Vergelijkt of isPressed true is
-  lights();                                   //Start de functie lights();
-  }else {
-    digitalWrite(YELLOW, HIGH);               //Zet LED geel aan
-    delay(500);                               //delay van een halve seconde
-    digitalWrite(YELLOW, LOW);                //Zet LED geel uit
-    delay(500);                               //delay van een halve seconde
+void setup() {
+  Serial.begin(9600); 
+  bluetooth.begin(9600); 
+
+  for (int i = 0; i < 3; i++) {
+    pinMode(LED[i], OUTPUT);
   }
 }
 
-void lights()                                 //functie lights aangemaakt
-{
-  Serial.println(buttonState);                //Print de value van buttonState
-  green_lights();                             //Start functie geen_lights()
-  delay(random(8000, 15000));                 //Start een random delay tussne 8 en 15 seconden
-  yellow_lights();                            //Start functie yellow_ligths()
-  delay(3000);                                //Start een delay van 3 seconden
-  red_lights();                               //Start functie red_lights()
-  delay(random(8000, 15000));                 //Start een random delay tussen 8 en 15 seconden
+void loop() { 
+  if (millis() - lastCheck > 100) {
+    lastCheck = millis();
+    if (bluetooth.available()) {
+      int startsign = bluetooth.read();
+      Serial.println(startsign);
+      if (startsign == 75) {
+        huidigeStaat = true;
+        lastTime = millis();
+      }
+    }
+  }
+  if (huidigeStaat) {
+    stopLicht();
+  } else {
+    storing();
+  }
 }
 
-void green_lights()                     //functie green_lihts aangemaakt
-{
-  digitalWrite(GREEN, HIGH);            //Zet LED groen aan
-  digitalWrite(YELLOW, LOW);            //Zet LED geel uit
-  digitalWrite(RED, LOW);               //Zet LED rood uit
+void storing() {
+  if (millis() - lastTime >= 1000) {
+    ledStatus = !ledStatus; // Toggle de LED status
+    digitalWrite(LED[1], ledStatus);
+    lastTime = millis();
+  }
 }
 
-void yellow_lights()                    //functie yellow_lights aangemaakt
-{ 
-  digitalWrite(GREEN, LOW);             //Zet LED groen uit
-  digitalWrite(YELLOW, HIGH);           //Zet LED geel aan
-  digitalWrite(RED, LOW);               //Zet LED rood uit
+void stopLicht() {
+  unsigned long elapsedTime = millis() - lastTime;
+n
+  if (elapsedTime <= 12000) {
+    setLeds(LOW, LOW, HIGH); // Groen
+  } else if (elapsedTime <= 18000) {
+    setLeds(LOW, HIGH, LOW); // Oranje
+  } else if (elapsedTime <= 30000) {
+    setLeds(HIGH, LOW, LOW); // Rood
+  } else {
+    setLeds(LOW, LOW, LOW); // Reset
+    huidigeStaat = false;
+  }
 }
 
-void red_lights()                       //functie red_lights aangemaakt
-{
-  digitalWrite(GREEN, LOW);             //Zet LED groen uit
-  digitalWrite(YELLOW, LOW);            //zet LED geel uit
-  digitalWrite(RED, HIGH);              //Zet LED rood aan
+void setLeds(int rood, int oranje, int groen) {
+  digitalWrite(LED[0], rood);
+  digitalWrite(LED[1], oranje);
+  digitalWrite(LED[2], groen);
 }
+
