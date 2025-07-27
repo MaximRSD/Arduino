0b1a7636fdb10f1ee885f409af86f9d1d5d64c2b
Maxim Jansen <45556790+MaximRSD@users.noreply.github.com>
2025-07-27 23:23:29 +0200

    Update opdracht9.ino

--- opdracht9.ino
+++ opdracht9.ino
@@ -1,69 +1,119 @@
-#include <SoftwareSerial.h>
+//elementen aangeven
+const int ledGreen = 4;
+const int ledYellow = 3;
+const int ledRed = 2;
+const int btnPin = 11;
 
-// Bluetooth op pinnen 0 en 1
-SoftwareSerial bluetooth(0, 1);
+int buttonState = 0;
+//ints waarin random nummer wordt opgeslagen
+int timeGreen;
+int timeRed;
 
-// LED-pinnen en variabelen
-const int LED[] = {4, 3, 2};
-unsigned long lastTime = 0;
-unsigned long lastCheck = 0;
-bool huidigeStaat = false;
-int ledStatus = LOW;
+int ledStatus = 0;
 
-void setup() {
-  Serial.begin(9600); 
-  bluetooth.begin(9600); 
+unsigned long timer;
+//bool of de knop is ingedrukt
+bool isPressed = false;
 
-  for (int i = 0; i < 3; i++) {
-    pinMode(LED[i], OUTPUT);
-  }
+void setup()
+{
+  //pinmodes instellen
+  pinMode(btnPin, INPUT);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  //serial aanzetten om println te lezen
+  Serial.begin(9600);
 }
 
-void loop() { 
-  if (millis() - lastCheck > 100) {
-    lastCheck = millis();
-    if (bluetooth.available()) {
-      int startsign = bluetooth.read();
-      Serial.println(startsign);
-      if (startsign == 75) {
-        huidigeStaat = true;
-        lastTime = millis();
-      }
+void loop()
+{
+  // Controleer Bluetooth seriële data
+  if (Serial.available() > 0) {
+    char incomingChar = Serial.read();
+    if (incomingChar == '0') {
+      isPressed = !isPressed;
+      timer = millis();
+      Serial.print("Bluetooth toggled isPressed: ");
+      Serial.println(isPressed);
     }
   }
-  if (huidigeStaat) {
-    stopLicht();
-  } else {
-    storing();
+
+  // Rest van je bestaande code hieronder...
+
+  unsigned long timeNow = millis();
+  buttonState = digitalRead(btnPin);
+
+  // Houd fysieke knop toggle ook intact
+  if(buttonState == HIGH)
+  {
+    if(isPressed == false)
+    {
+      isPressed = true;
+      timer = millis();
+    }
+    else
+    {
+      isPressed = false;
+      timer = millis();
+    }
+    Serial.println(isPressed);
   }
-}
 
-void storing() {
-  if (millis() - lastTime >= 1000) {
-    ledStatus = !ledStatus; // Toggle de LED status
-    digitalWrite(LED[1], ledStatus);
-    lastTime = millis();
+  if (isPressed == true)
+  {
+    Serial.println("in ispressed");
+    timeGreen = random(8000,15000);
+    timeRed = random(8000,15000);
+
+    if(timeNow - timer <= timeGreen){
+      Groen();
+    } else if(timeNow - timer <= (timeGreen + 3000)){
+      Geel();
+    } else if(timer != 0 && timeNow - timer <= (timeGreen + 3000 + timeRed)){
+      Rood();
+    } else {
+      Niks();
+    }
   }
-}
-
-void stopLicht() {
-  unsigned long elapsedTime = millis() - lastTime;
-n
-  if (elapsedTime <= 12000) {
-    setLeds(LOW, LOW, HIGH); // Groen
-  } else if (elapsedTime <= 18000) {
-    setLeds(LOW, HIGH, LOW); // Oranje
-  } else if (elapsedTime <= 30000) {
-    setLeds(HIGH, LOW, LOW); // Rood
-  } else {
-    setLeds(LOW, LOW, LOW); // Reset
-    huidigeStaat = false;
+  else
+  {
+    if(timeNow - timer >= 1000){
+      Serial.println("in else");
+      timer = timeNow;
+      ledStatus = (ledStatus == LOW) ? HIGH : LOW;
+      digitalWrite(ledYellow, ledStatus);
+    }
   }
+
+  delay(300);
 }
 
-void setLeds(int rood, int oranje, int groen) {
-  digitalWrite(LED[0], rood);
-  digitalWrite(LED[1], oranje);
-  digitalWrite(LED[2], groen);
+//lightjes aan en uit zetten
+void Groen()
+{
+  digitalWrite(ledGreen, HIGH);
+  digitalWrite(ledYellow, LOW);
+  digitalWrite(ledRed, LOW);
 }
 
+void Geel()
+{
+  digitalWrite(ledGreen, LOW);
+  digitalWrite(ledYellow, HIGH);
+  digitalWrite(ledRed, LOW);
+}
+
+void Rood()
+{
+  digitalWrite(ledGreen, LOW);
+  digitalWrite(ledYellow, LOW);
+  digitalWrite(ledRed, HIGH);
+}
+void Niks()
+{
+  digitalWrite(ledGreen, LOW);
+  digitalWrite(ledYellow, LOW);
+  digitalWrite(ledRed, LOW);
+}
+  
